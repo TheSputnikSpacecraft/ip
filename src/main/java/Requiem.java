7d919db69147d0f4d5d058193feac5563c3e17f8
Rajeshprabu Sidharth <78141380+TheSputnikSpacecraft@users.noreply.github.com>
2026-01-21 19:00:29 +0800

    Requiem.java: Rename main class and add greeting
    Rename Duke to Requiem to personalize the application.
    
    Implement greet and exit messages to handle initial interaction.
    
    Update documentation and scripts to remove legacy Duke references.

---
+++ src/main/java/Requiem.java
@@ -0,0 +1,13 @@
+public class Requiem {
+    public static void main(String[] args) {
+        String greeting = "____________________________________________________________\n" +
+                " Hello! I'm Requiem\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________";
+        String exit = " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+
+        System.out.println(greeting);
+        System.out.println(exit);
+    }
+}
