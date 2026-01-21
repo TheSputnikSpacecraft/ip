debb7161f765c9d6b434db505859f17c96e07473
Rajeshprabu Sidharth <78141380+TheSputnikSpacecraft@users.noreply.github.com>
2026-01-21 20:37:46 +0800

    A-Enums: Use Enums for command handling
    Introduce Command enum to replace string literals.
    
    Refactor Requiem.java to parse user input into Command constants.
    
    Switch to switch-case structure for cleaner logic flow.
    
    Maintain existing error handling behavior.

---
+++ src/main/java/Command.java
@@ -0,0 +1,13 @@
+/**
+ * Represents the commands supported by the Requiem chatbot.
+ */
+public enum Command {
+    BYE,
+    LIST,
+    MARK,
+    UNMARK,
+    TODO,
+    DEADLINE,
+    EVENT,
+    DELETE
+}
--- src/main/java/Requiem.java
+++ src/main/java/Requiem.java
@@ -31,90 +31,107 @@ public class Requiem {
             System.out.println("____________________________________________________________");
 
             try {
-                if (input.equals("bye")) {
-                    System.out.println(exit);
-                    break;
-                } else if (input.equals("list")) {
-                    System.out.println(" Here are the tasks in your list:");
-                    for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println(" " + (i + 1) + "." + tasks.get(i));
-                    }
-                    System.out.println("____________________________________________________________");
-                } else if (input.startsWith("mark ")) {
-                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                    tasks.get(index).markAsDone();
-                    System.out.println(" Nice! I've marked this task as done:");
-                    System.out.println("   " + tasks.get(index));
-                    System.out.println("____________________________________________________________");
-                } else if (input.startsWith("unmark ")) {
-                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                    tasks.get(index).markAsUndone();
-                    System.out.println(" OK, I've marked this task as not done yet:");
-                    System.out.println("   " + tasks.get(index));
-                    System.out.println("____________________________________________________________");
-                } else if (input.startsWith("todo")) {
-                    if (input.trim().equals("todo")) {
-                        throw new RequiemException("The description of a todo cannot be empty.");
-                    }
-                    String description = input.substring(5).trim();
-                    if (description.isEmpty()) {
-                        throw new RequiemException("The description of a todo cannot be empty.");
-                    }
-                    Task newTask = new Todo(description);
-                    tasks.add(newTask);
-                    System.out.println(" Got it. I've added this task:");
-                    System.out.println("   " + newTask);
-                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
-                    System.out.println("____________________________________________________________");
-                } else if (input.startsWith("deadline")) {
-                    if (input.trim().equals("deadline")) {
-                        throw new RequiemException("The description of a deadline cannot be empty.");
-                    }
-                    int byIndex = input.indexOf("/by");
-                    if (byIndex == -1) {
-                        throw new RequiemException("The deadline must have a /by time.");
-                    }
-                    String description = input.substring(9, byIndex).trim();
-                    if (description.isEmpty()) {
-                        throw new RequiemException("The description of a deadline cannot be empty.");
-                    }
-                    String by = input.substring(byIndex + 3).trim();
-                    Task newTask = new Deadline(description, by);
-                    tasks.add(newTask);
-                    System.out.println(" Got it. I've added this task:");
-                    System.out.println("   " + newTask);
-                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
-                    System.out.println("____________________________________________________________");
-                } else if (input.startsWith("event")) {
-                    if (input.trim().equals("event")) {
-                        throw new RequiemException("The description of an event cannot be empty.");
-                    }
-                    int fromIndex = input.indexOf("/from");
-                    int toIndex = input.indexOf("/to");
-                    if (fromIndex == -1 || toIndex == -1) {
-                        throw new RequiemException("The event must have /from and /to times.");
-                    }
-                    String description = input.substring(6, fromIndex).trim();
-                    if (description.isEmpty()) {
-                        throw new RequiemException("The description of an event cannot be empty.");
-                    }
-                    String from = input.substring(fromIndex + 5, toIndex).trim();
-                    String to = input.substring(toIndex + 3).trim();
-                    Task newTask = new Event(description, from, to);
-                    tasks.add(newTask);
-                    System.out.println(" Got it. I've added this task:");
-                    System.out.println("   " + newTask);
-                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
-                    System.out.println("____________________________________________________________");
-                } else if (input.startsWith("delete ")) {
-                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
-                    Task removedTask = tasks.remove(index);
-                    System.out.println(" Noted. I've removed this task:");
-                    System.out.println("   " + removedTask);
-                    System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
-                    System.out.println("____________________________________________________________");
-                } else {
+                String[] words = input.split(" ", 2);
+                Command command;
+                try {
+                    command = Command.valueOf(words[0].toUpperCase());
+                } catch (IllegalArgumentException e) {
                     throw new RequiemException("I'm sorry, but I don't know what that means :-(");
+                }
+
+                switch (command) {
+                    case BYE:
+                        System.out.println(exit);
+                        scanner.close();
+                        return;
+                    case LIST:
+                        System.out.println(" Here are the tasks in your list:");
+                        for (int i = 0; i < tasks.size(); i++) {
+                            System.out.println(" " + (i + 1) + "." + tasks.get(i));
+                        }
+                        System.out.println("____________________________________________________________");
+                        break;
+                    case MARK:
+                        if (words.length < 2)
+                            throw new RequiemException("You need to specify a task index to mark.");
+                        int markIndex = Integer.parseInt(words[1]) - 1;
+                        tasks.get(markIndex).markAsDone();
+                        System.out.println(" Nice! I've marked this task as done:");
+                        System.out.println("   " + tasks.get(markIndex));
+                        System.out.println("____________________________________________________________");
+                        break;
+                    case UNMARK:
+                        if (words.length < 2)
+                            throw new RequiemException("You need to specify a task index to unmark.");
+                        int unmarkIndex = Integer.parseInt(words[1]) - 1;
+                        tasks.get(unmarkIndex).markAsUndone();
+                        System.out.println(" OK, I've marked this task as not done yet:");
+                        System.out.println("   " + tasks.get(unmarkIndex));
+                        System.out.println("____________________________________________________________");
+                        break;
+                    case TODO:
+                        if (words.length < 2 || words[1].trim().isEmpty()) {
+                            throw new RequiemException("The description of a todo cannot be empty.");
+                        }
+                        Task todo = new Todo(words[1].trim());
+                        tasks.add(todo);
+                        System.out.println(" Got it. I've added this task:");
+                        System.out.println("   " + todo);
+                        System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                        System.out.println("____________________________________________________________");
+                        break;
+                    case DEADLINE:
+                        if (words.length < 2 || words[1].trim().isEmpty()) {
+                            throw new RequiemException("The description of a deadline cannot be empty.");
+                        }
+                        int byIndex = input.indexOf("/by");
+                        if (byIndex == -1) {
+                            throw new RequiemException("The deadline must have a /by time.");
+                        }
+                        String deadlineDesc = input.substring(9, byIndex).trim();
+                        if (deadlineDesc.isEmpty()) {
+                            throw new RequiemException("The description of a deadline cannot be empty.");
+                        }
+                        String by = input.substring(byIndex + 3).trim();
+                        Task deadline = new Deadline(deadlineDesc, by);
+                        tasks.add(deadline);
+                        System.out.println(" Got it. I've added this task:");
+                        System.out.println("   " + deadline);
+                        System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                        System.out.println("____________________________________________________________");
+                        break;
+                    case EVENT:
+                        if (words.length < 2 || words[1].trim().isEmpty()) {
+                            throw new RequiemException("The description of an event cannot be empty.");
+                        }
+                        int fromIndex = input.indexOf("/from");
+                        int toIndex = input.indexOf("/to");
+                        if (fromIndex == -1 || toIndex == -1) {
+                            throw new RequiemException("The event must have /from and /to times.");
+                        }
+                        String eventDesc = input.substring(6, fromIndex).trim();
+                        if (eventDesc.isEmpty()) {
+                            throw new RequiemException("The description of an event cannot be empty.");
+                        }
+                        String from = input.substring(fromIndex + 5, toIndex).trim();
+                        String to = input.substring(toIndex + 3).trim();
+                        Task event = new Event(eventDesc, from, to);
+                        tasks.add(event);
+                        System.out.println(" Got it. I've added this task:");
+                        System.out.println("   " + event);
+                        System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                        System.out.println("____________________________________________________________");
+                        break;
+                    case DELETE:
+                        if (words.length < 2)
+                            throw new RequiemException("You need to specify a task index to delete.");
+                        int deleteIndex = Integer.parseInt(words[1]) - 1;
+                        Task removedTask = tasks.remove(deleteIndex);
+                        System.out.println(" Noted. I've removed this task:");
+                        System.out.println("   " + removedTask);
+                        System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+                        System.out.println("____________________________________________________________");
+                        break;
                 }
             } catch (RequiemException e) {
                 System.out.println(" Yare Yare Daze " + e.getMessage());
@@ -125,6 +142,5 @@ public class Requiem {
             }
         }
 
-        scanner.close();
     }
 }
