86c65a585421a8fde1247f4ff6c5bc9d1a3a409a
Rajeshprabu Sidharth <78141380+TheSputnikSpacecraft@users.noreply.github.com>
2026-01-28 22:50:31 +0800

    Fix: remove duplicate java files created after rebase

--- src/main/java/RequiemException.java
+++
@@ -1,8 +0,0 @@
-/**
- * Represents exceptions specific to the Requiem chatbot.
- */
-public class RequiemException extends Exception {
-    public RequiemException(String message) {
-        super(message);
-    }
-}
